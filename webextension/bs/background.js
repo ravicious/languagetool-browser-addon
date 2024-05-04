1764f18f3a395751071ec1c9126667844672c818
Rafał Cieślak <ravicious@gmail.com>
2024-05-04 19:30:49 +0200

    Shorten context menu text

--- webextension/bs/background.js
+++ webextension/bs/background.js
@@ -29,8 +29,7 @@ if (chrome && chrome.browserAction && chrome.browserAction.openPopup) {
       if (commands && commands.length && commands.length > 0 && commands[0].shortcut) {
         shortcut = commands[0].shortcut;
       }
-      // there seems to be no better way to show the shortcut (https://bugs.chromium.org/p/chromium/issues/detail?id=142840):
-      const title = shortcut ? chrome.i18n.getMessage("contextMenuItemWithShortcut", shortcut) : chrome.i18n.getMessage("contextMenuItem");
+      const title = chrome.i18n.getMessage("contextMenuItem");
       chrome.contextMenus.create({"title": title, "contexts":["selection", "editable"], "id": "contextLT"});
     });
     // With an entry only for 'editable' we could have a better name, but then Chrome will
